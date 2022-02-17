2f48c579efb21f991b6479c6c3f40506106003da
Abtinz <abtinzandi@gmail.com>
2022-02-17 14:11:41 +0330

    templates and render

--- firstProject/BlogApp/views.py
+++ firstProject/BlogApp/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello Django </br>Abtin Zandi : 19 : Theran </br>This is Home page for path test in django!")
+    return render(request , "BlogApp/home.html")
 
 def contact(request):
-    return HttpResponse("Contact us : </br>GitHub : Abtinz </br> Email : NotAbtin@FakeEmail.com" )
+    return render(request,"BlogApp/contact.html")
 
 def account(request , username):
-    return HttpResponse(f"Account:</br>UserName:{username}</br>Hi {username}</br>this is your django profile" )
+    return render(request,"BlogApp/account.html")
+    #return HttpResponse(f"Account:</br>UserName:{username}</br>Hi {username}</br>this is your django profile" )
