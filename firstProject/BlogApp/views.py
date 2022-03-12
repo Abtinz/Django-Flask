9b1c32f5d9d9fe0c9372e03a90cf322497b2b5b4
Abtinz <abtinzandi@gmail.com>
2022-03-12 23:53:11 +0330

    //GET details from requets to sign up a new user

--- firstProject/BlogApp/views.py
+++ firstProject/BlogApp/views.py
@@ -6,8 +6,22 @@ def home(request):
 def contact(request):
     return render(request,"BlogApp/contact.html")
 
-
 def new_account(request):
+    full_name = request.GET.get("full_name")
+    username = request.GET.get("username")
+    password = request.GET.get("password")
+    email = request.GET.get("email")
+    Bio = request.GET.get("Bio")
+    if full_name and username :
+        if password and email and Bio:
+            user_information.objects.create(full_name = full_name ,
+                                              username = username , 
+                                              password = password ,
+                                              email = email ,
+                                              Bio = Bio
+                                              )
+            user = user_information.objects.get(username = username)
+            return render(request,"BlogApp/account.html" , context={"account" : user})
     return render(request,"BlogApp/new_account.html")
 
 def account(request , username):
