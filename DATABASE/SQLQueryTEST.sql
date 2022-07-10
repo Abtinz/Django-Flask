7d515839d96ad5364cc4c65c52f450d535b9c875
Abtinz <abtinzandi@gmail.com>
2022-07-10 18:27:21 +0430

    conditional selection in sql

--- DATABASE/SQLQueryTEST.sql
+++ DATABASE/SQLQueryTEST.sql
@@ -15,5 +15,25 @@ SELECT * FROM Person.Address
 -- specific column ' ' from table
 SELECT City FROM Person.Address
 GO 
-
 SELECT StateProvinceID AS StateIdNumber FROM Person.Address
+
+
+GO 
+-- conditional selection
+
+SELECT * FROM HumanResources.Employee 
+WHERE SickLeaveHours > 50
+
+GO
+SELECT * FROM HumanResources.Employee 
+WHERE BusinessEntityID % 2 = 0 ;
+SELECT * FROM HumanResources.Employee 
+WHERE BusinessEntityID BETWEEN 40 AND 80 ;
+
+GO
+SELECT * FROM HumanResources.Employee 
+WHERE Gender  = 'M' ;
+
+--REMOVE REPEATED DATA
+GO 
+SELECT DISTINCT StoreID FROM Sales.Customer
