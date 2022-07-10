1072bccb034e6e800262647888031e73fa1c87ff
Abtinz <abtinzandi@gmail.com>
2022-07-10 19:00:19 +0430

    LIKE IN SQL 3 SPECIFIC CHAR INDEX BY ( _ )

--- DATABASE/like_Tsql.sql
+++ DATABASE/like_Tsql.sql
@@ -8,3 +8,15 @@ WHERE Name LIKE 'P%'
 --end with ...
 Select * FROM HumanResources.Department
 WHERE Name LIKE '%ing'
+
+--SECOND CHAR IS ...
+Select * FROM HumanResources.Department
+WHERE Name LIKE '_a%'
+
+--THIRD CHAR IS ...
+Select * FROM HumanResources.Department
+WHERE Name LIKE '__m%'
+
+--SECOND CHAR FROM LASTS CHARACTERS IS ...
+Select * FROM HumanResources.Department
+WHERE Name LIKE '%n_'
