09abb46e0e7c32de2cff45b23ade00d842026d22
Abtinz <abtinzandi@gmail.com>
2022-07-10 18:07:31 +0430

    sql query use for import db select for tables

---
+++ DATABASE/SQLQueryTEST.sql
@@ -0,0 +1,14 @@
+use AdventureWorks2016
+
+GO
+
+select 'Test'
+
+GO 
+
+-- AS --> COLUMNS NAME
+SELECT 'SECOND TEST' AS 'COLUMN TEST'
+
+GO
+-- * IS FOR ALL COLUMNS OF TABLE
+SELECT * FROM Person.Address
