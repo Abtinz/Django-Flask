3535b721453e16e01fa3ff814c8bb201124bda49
Abtinz <abtinzandi@gmail.com>
2022-07-10 18:13:01 +0430

    select special column of table

--- DATABASE/SQLQueryTEST.sql
+++ DATABASE/SQLQueryTEST.sql
@@ -12,3 +12,8 @@ SELECT 'SECOND TEST' AS 'COLUMN TEST'
 GO
 -- * IS FOR ALL COLUMNS OF TABLE
 SELECT * FROM Person.Address
+-- specific column ' ' from table
+SELECT City FROM Person.Address
+GO 
+
+SELECT StateProvinceID AS StateIdNumber FROM Person.Address
