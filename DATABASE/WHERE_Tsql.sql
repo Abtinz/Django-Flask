a359fcbc58c095fd46e748eda8201ad89323a4e8
Abtinz <abtinzandi@gmail.com>
2022-07-10 18:42:41 +0430

    NOTEQUAL IN SQL <>

--- DATABASE/WHERE_Tsql.sql
+++ DATABASE/WHERE_Tsql.sql
@@ -4,4 +4,14 @@ GO
 --sorting
 Select VacationHours FROM HumanResources.Employee
 WHERE VacationHours > 30 
-ORDER BY SickLeaveHours desc
+ORDER BY SickLeaveHours desc;
+
+GO
+Select VacationHours FROM HumanResources.Employee
+WHERE VacationHours > 30 AND Gender = 'M'
+ORDER BY VacationHours desc;
+
+GO
+--NOTEQUAL IN SQL
+Select * FROM HumanResources.Employee
+WHERE  NationalIDNumber <> 295847284
