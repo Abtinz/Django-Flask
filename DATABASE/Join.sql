063f0a6120d39a1fa2741ebb75e08fa54467a358
Abtinz <abtinzandi@gmail.com>
2022-07-12 18:37:58 +0430

    RIGHT AND LEFT JION when you need to select fathers key value wich has child or not --> right join when you need to select child value wich has fathers key or not --> left join

--- DATABASE/Join.sql
+++ DATABASE/Join.sql
@@ -9,3 +9,13 @@ GO
 SELECT MajorTypeID , MajorTitle FROM Persons
 INNER JOIN Majors 
 ON Persons.MajorTypeID = Majors.MajorID
+
+GO
+SELECT MajorTypeID , MajorTitle FROM Persons
+LEFT JOIN Majors 
+ON Persons.MajorTypeID = Majors.MajorID
+
+GO
+SELECT MajorTypeID , MajorTitle FROM Persons
+RIGHT JOIN Majors 
+ON Persons.MajorTypeID = Majors.MajorID
