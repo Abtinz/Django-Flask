ca0263ec6537fe335dd35ed052ef38eda2510e25
Abtinz <abtinzandi@gmail.com>
2022-07-15 22:54:17 +0430

    NOT NULL and Primary key in T_SQL

--- DATABASE/CreateTable_Tsql.sql
+++ DATABASE/CreateTable_Tsql.sql
@@ -5,3 +5,19 @@ CREATE TABLE SqlServerCourse(
 		SureName NVARCHAR(20) ,
 		LastName NVARCHAR(20)
 );
+
+CREATE TABLE APCourse(
+        PersonID INT ,
+		SureName NVARCHAR(20) ,
+		LastName NVARCHAR(20) ,
+
+		PRIMARY KEY(PersonID)
+);
+
+CREATE TABLE DataStructureCourse(
+        PersonID INT ,
+		SureName NVARCHAR(20) NOT NULL,
+		LastName NVARCHAR(20) NOT NULL,
+
+		PRIMARY KEY(PersonID)
+);
