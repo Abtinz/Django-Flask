0690fe1aab323146025a932f9910c7a3c96dbe7b
Abtinz <abtinzandi@gmail.com>
2022-07-15 22:26:13 +0430

    INSERT DATAS IN TABLE

---
+++ DATABASE/Insert.sql
@@ -0,0 +1,15 @@
+GO
+INSERT INTO Majors
+VALUES('Computer Engineering')
+
+GO
+INSERT INTO Courses(CourseTitle)
+VALUES('Artificial Intelligence')
+
+GO
+INSERT INTO Courses
+VALUES('Deep Learning',10)
+
+GO
+INSERT INTO Courses
+VALUES('SQL',1)
