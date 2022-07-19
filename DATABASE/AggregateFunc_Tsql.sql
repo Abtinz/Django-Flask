03084d31940f3196bc45499517a1e7adc888a076
Abtinz <abtinzandi@gmail.com>
2022-07-19 23:10:50 +0430

    count and condition

--- DATABASE/AggregateFunc_Tsql.sql
+++ DATABASE/AggregateFunc_Tsql.sql
@@ -3,3 +3,8 @@ use AdventureWorks2016
 GO
 SELECT COUNT(NationalIDNumber) AS 'Employee Count'
 FROM HumanResources.Employee
+
+GO
+SELECT COUNT(VacationHours) AS 'Lazy Employee Count'
+FROM HumanResources.Employee
+WHERE VacationHours > 40
