2bf6ff07c8db5931e9360b32ba6d7deb54cf76eb
Abtinz <abtinzandi@gmail.com>
2022-07-19 22:20:32 +0430

    how to make a column nullable in sql

--- DATABASE/Alter_Tsql.sql
+++ DATABASE/Alter_Tsql.sql
@@ -7,3 +7,7 @@ ADD Description NVARCHAR(150)
 GO
 ALTER TABLE Courses
 ADD CourseInstituation NVARCHAR(30) NOT NULL DEFAULT 'SQL & DS'
+
+GO
+ALTER TABLE Courses
+ALTER COLUMN CourseInstituation NVARCHAR(30) NULL
