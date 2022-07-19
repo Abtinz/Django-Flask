c6ce84e23b140642b169ef0945dd17a70e79f10c
Abtinz <abtinzandi@gmail.com>
2022-07-19 22:24:17 +0430

    How to change column name in sql

--- DATABASE/Alter_Tsql.sql
+++ DATABASE/Alter_Tsql.sql
@@ -4,10 +4,15 @@ ALTER TABLE Courses
 --Adding a column
 ADD Description NVARCHAR(150)
 
+
 GO
 ALTER TABLE Courses
 ADD CourseInstituation NVARCHAR(30) NOT NULL DEFAULT 'SQL & DS'
 
+--how to make a column nullable in sql
 GO
 ALTER TABLE Courses
 ALTER COLUMN CourseInstituation NVARCHAR(30) NULL
+
+GO
+SP_RENAME 'Courses.CourseInstituation' , 'Instituation' , 'COLUMN'
