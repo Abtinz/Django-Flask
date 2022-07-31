ef813499b610320e5094c164abcebb403d6e6363
Abtinz <abtinzandi@gmail.com>
2022-07-31 20:55:23 +0430

    view.py --> this is how we make our views httpresponse -->help us to send letter

--- Django/Intro/Football_Club/clubs_app/views.py
+++ Django/Intro/Football_Club/clubs_app/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse('welcom to football club app')
