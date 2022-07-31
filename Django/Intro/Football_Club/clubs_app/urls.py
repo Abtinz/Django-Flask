08e5a3503c0671673ca110dcbe7112990e14ca65
Abtinz <abtinzandi@gmail.com>
2022-07-31 21:17:03 +0430

    new view

--- Django/Intro/Football_Club/clubs_app/urls.py
+++ Django/Intro/Football_Club/clubs_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.home)
+    path('home', views.home),
+    path('information/table' , views.clubs_information)
 ]
--- Django/Intro/Football_Club/clubs_app/views.py
+++ Django/Intro/Football_Club/clubs_app/views.py
@@ -1,4 +1,8 @@
+from django.http import Http404
 from django.shortcuts import render , HttpResponse
 
 def home(request):
     return HttpResponse('welcom to football club app')
+
+def clubs_information(request):
+    raise Http404
