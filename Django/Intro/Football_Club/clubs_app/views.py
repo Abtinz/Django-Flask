ccc1e504e9ebedcbcd794bd17b22f77378553aa8
Abtinz <abtinzandi@gmail.com>
2022-08-09 20:22:09 +0430

    Clube.objects.all() --> will give us all objects from club table

--- Django/Intro/Football_Club/clubs_app/views.py
+++ Django/Intro/Football_Club/clubs_app/views.py
@@ -1,6 +1,6 @@
 from django.http import Http404
 from django.shortcuts import render , HttpResponse
-
+from .models import Clube
 def home(request):
     return HttpResponse('welcom to football club app')
 
@@ -8,6 +8,7 @@ def clubs_table(request):
     raise Http404
 
 def club_information(request,club_name):
+        clubs = Clube.objects.all()
         return render(request , 'clubs_app/football_club_profile.html' ,  context={'club_name':club_name})
 
     
