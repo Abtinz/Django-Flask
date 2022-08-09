f90d81386e6ae9a18e80fcc063dfcc2d1f08f7a6
Abtinz <abtinzandi@gmail.com>
2022-08-09 20:34:07 +0430

    rehears !

--- Django/Intro/Football_Club/clubs_app/views.py
+++ Django/Intro/Football_Club/clubs_app/views.py
@@ -4,13 +4,12 @@ from .models import Clube
 def home(request):
     return HttpResponse('welcom to football club app')
 
-def clubes_table(request):
+def clubs_table(request):
     raise Http404
 
-def clube_information(request,club_name):
-        clubes = Clube.objects.all()
-        if club_name in clubes:
-            return render(request , 'clubs_app/football_club_profile.html' ,  context={'club':Clube.objects.get(name = club_name)})
-        #else :
+def club_information(request,club_name):
+        club = Clube.objects.get(name = club_name)
+        return render(request , 'clubs_app/football_club_profile.html' ,  context={'club':club})
+        
 
     
