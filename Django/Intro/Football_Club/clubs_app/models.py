a01699a6be771a3d506c0715ab361a9f5e20aee5
Abtinz <abtinzandi@gmail.com>
2022-08-09 19:52:12 +0430

    country and league_title added

--- Django/Intro/Football_Club/clubs_app/models.py
+++ Django/Intro/Football_Club/clubs_app/models.py
@@ -4,4 +4,5 @@ class Clube(models.Model):
      name = models.CharField(max_length = 30)
      foundation_date = models.DateField()
      trophies_count = models.IntegerField(default=0)
-     
+     league_title = models.CharField(max_length = 30)
+     country = models.CharField(max_length = 30)
