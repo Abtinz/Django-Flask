fff614e80a9bc532a39cb9d6c96bc2c48c0abce0
Abtinz <abtinzandi@gmail.com>
2022-08-09 19:51:02 +0430

    IntField

--- Django/Intro/Football_Club/clubs_app/models.py
+++ Django/Intro/Football_Club/clubs_app/models.py
@@ -3,4 +3,5 @@ from django.db import models
 class Clube(models.Model):
      name = models.CharField(max_length = 30)
      foundation_date = models.DateField()
+     trophies_count = models.IntegerField(default=0)
      
