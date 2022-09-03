da20e599e54e6381b8d0a26ddd5dc69e9fef8647
Abtinz <abtinzandi@gmail.com>
2022-09-03 21:53:49 +0430

    MEDIA_URL = '/img/' is a way to set our madia files directory in our django project

--- Django/Intro/Football_Club/Football_Club/settings.py
+++ Django/Intro/Football_Club/Football_Club/settings.py
@@ -122,6 +122,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     path.join(BASE_DIR,'assets')
 ]
+MEDIA_URL = '/img/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
