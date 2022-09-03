136bced0bcf58a0a859c9168874e03a45b9aec0f
Abtinz <abtinzandi@gmail.com>
2022-09-03 21:08:49 +0430

    simple design for form!

--- Django/Intro/Football_Club/clubs_app/models.py
+++ Django/Intro/Football_Club/clubs_app/models.py
@@ -4,11 +4,11 @@ from django.db import models
 class Clube(models.Model):
      name = models.CharField(max_length = 30)
      foundation_date = models.DateField()
-     trophies_count = models.IntegerField(default=0)
+     trophies_count = models.IntegerField(default=0) ###
      league_title = models.CharField(max_length = 30)
      country = models.CharField(max_length = 30)
      rank = models.IntegerField()
-     situation =  models.CharField(max_length = 30 ,default="OnFire")
-     club_logo = models.ImageField(default = "img\default.png")
+     situation =  models.CharField(max_length = 30 ,default="OnFire") ###
+     club_logo = models.ImageField(default = "img\default.png") ###
      def __str__(self):
           return self.name
