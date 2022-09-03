838cf5fdaa20494df84e124fc20170a3bb344bc0
Abtinz <abtinzandi@gmail.com>
2022-09-03 20:53:58 +0430

    set the new path

--- Django/Intro/Football_Club/clubs_app/urls.py
+++ Django/Intro/Football_Club/clubs_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('home', views.home),
     path('information/table' , views.clubs_table),
+    path('creatnewclub' , views.new_football_club),
     path('<club_name>/information' , views.club_information)
 ]
