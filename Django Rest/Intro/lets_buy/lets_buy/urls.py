667c4a514adc5912f2485a293edf34f7bec5a8e9
Abtinz <abtinzandi@gmail.com>
2024-01-31 00:21:29 +0330

    include(api.urls) will includ all api urls to the main project

--- Django Rest/Intro/lets_buy/lets_buy/urls.py
+++ Django Rest/Intro/lets_buy/lets_buy/urls.py
@@ -19,5 +19,6 @@ from django.urls import path , include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("api.urls"))
+    path("", include("api.urls")),
+    path("", include("product.urls"))
 ]
