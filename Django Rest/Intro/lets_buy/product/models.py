419eb8367e402e01262b862d4a10ca15a02a8509
Abtinz <abtinzandi@gmail.com>
2024-01-31 16:43:24 +0330

    validators

--- Django Rest/Intro/lets_buy/product/models.py
+++ Django Rest/Intro/lets_buy/product/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.core.validators import MaxValueValidator , MinValueValidator
 
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length = "200", unique = True)
     original_price = models.FloatField()
-    discount = models.FloatField()
-    
+    discount = models.IntegerField(
+        default  = 0,
+        validators = [MinValueValidator(0), MaxValueValidator(100)]
+    )
+
