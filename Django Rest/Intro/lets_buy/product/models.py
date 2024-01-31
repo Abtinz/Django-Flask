3d351bb2a4f0832624e21221fb61b946d7f00c20
Abtinz <abtinzandi@gmail.com>
2024-01-31 16:41:12 +0330

    simple class for db

--- Django Rest/Intro/lets_buy/product/models.py
+++ Django Rest/Intro/lets_buy/product/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length = "200", unique = True)
+    original_price = models.FloatField()
+    discount = models.FloatField()
+    
