64b741eb4dac76029c08ffd69b0ad92785219e81
Abtinz <abtinzandi@gmail.com>
2024-02-01 22:56:12 +0330

    urls

--- Django Rest/WebSocket/wechatapp/wechatapp/settings.py
+++ Django Rest/WebSocket/wechatapp/wechatapp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -56,7 +56,9 @@ ROOT_URLCONF = "wechatapp.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            os.path.join(BASE_DIR , "templates"),
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
