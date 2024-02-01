f7f29087a0de338d0daa846f9a8fd85431678b8a
Abtinz <abtinzandi@gmail.com>
2024-02-01 21:20:05 +0330

    ddose not exist

--- Django Rest/Intro/lets_buy/product/urls.py
+++ Django Rest/Intro/lets_buy/product/urls.py
@@ -4,5 +4,6 @@ from product import views
 
 
 urlpatterns =  [
-    path("product/" , views.ProductViews.as_view(), name = "product_view")
+    path("product/" , views.ProductViews.as_view(), name = "product_view"),
+    path("product/<id>/" , views.SingleProductViews.as_view(), name = "single_product_view")
 ]
--- Django Rest/Intro/lets_buy/product/views.py
+++ Django Rest/Intro/lets_buy/product/views.py
@@ -12,3 +12,20 @@ class ProductViews(APIView):
         queryset = Product.objects.all()
         serializer = ProductSerializers(queryset , many = True)
         return Response(serializer.data)
+    
+class SingleProductViews(APIView):
+
+    def is_exist(self , id):
+        try:
+            queryset = Product.objects.get(id = id)
+            return queryset
+        except Product.DoesNotExist:
+            pass
+    
+    def get(self , request, id):
+        queryset = self.is_exist(id= id)
+        if queryset:
+            serializer = ProductSerializers(queryset)
+            return Response(serializer.data)
+        else:
+            return Response({"message":"no items with this id ..."})
