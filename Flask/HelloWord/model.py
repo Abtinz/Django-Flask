75a0e31c8656fb35bbc6c3aa1527169af86e4997
Abtinz <abtinzandi@gmail.com>
2024-04-03 19:04:58 +0330

    SQLALCHEMY_DATABASE_URI will set via given url (in memory or cloud paths) in app configs

---
+++ Flask/HelloWord/model.py
@@ -0,0 +1,7 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+#SQLALCHEMY_DATABASE_URI will set via given url (in memory or cloud paths) in app configs
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movies.db"
+
