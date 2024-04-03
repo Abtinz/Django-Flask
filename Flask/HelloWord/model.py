baf7143e52d823e6679692389bcd2f3721d70254
Abtinz <abtinzandi@gmail.com>
2024-04-03 19:06:11 +0330

    ORM Creation vie WSGI configs

--- Flask/HelloWord/model.py
+++ Flask/HelloWord/model.py
@@ -5,3 +5,5 @@ app = Flask(__name__)
 #SQLALCHEMY_DATABASE_URI will set via given url (in memory or cloud paths) in app configs
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movies.db"
 
+#ORM Creation vie WSGI configs
+db = SQLAlchemy(app= app)
