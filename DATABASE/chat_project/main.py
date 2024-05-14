a234201c8f9ddd9053a6ad40baac1972150101f9
Abtinz <abtinzandi@gmail.com>
2024-05-14 21:31:18 +0330

    table creation api

--- DATABASE/chat_project/main.py
+++ DATABASE/chat_project/main.py
@@ -1,27 +1,25 @@
 import json
 from flask import Flask, abort, request, jsonify
 
+from postgres import PostgresService
+
 
 app = Flask(__name__)
 
-@app.route('/api/initialization', methods=['GET'])
-def initialization():
+@app.route('/api/tables/create', methods=['GET'])
+def create_tables():
 
     try:
-
-        
-            return abort(404,{'message': 'no results for given name in imdb, elasticsearch system and redis'})
-
+        postgresService.create_table()
+        return abort(200,{'message': 'database is created successfully'})
 
     except Exception as error:
-        
-
-        return abort(404,{'message': 'no results for given name in imdb, elasticsearch system and redis'})
+        return abort(404,{'message': 'connection failed in database system'})
 
         
 
 if __name__ == '__main__':
     
     #initializing the postgres database system
-    
+    postgresService = PostgresService()
     app.run(debug=False, host='0.0.0.0', port=5000)
--- DATABASE/chat_project/postgres.py
+++ DATABASE/chat_project/postgres.py
@@ -2,6 +2,61 @@ import psycopg2
 
 class PostgresService():
     def __init__(self) -> None:
-        self.conn = psycopg2.connect(
+        try:
+            self.conn = psycopg2.connect(
+            dbname = "postgres",
+            user = "admin",
+            password = "12345",
+            host = "localhost",
+            port = "5432"
+            )
+
+            print("database is created on 5432.port.postgres ")
+        except Exception as error:
+            print(error)
             
-        )
+
+    def create_user_db(self):
+
+        cursor = self.conn.cursor()
+        cursor.execute("CREATE TABLE Account (ID INT PRIMARY KEY,name VARCHAR(255),fullname VARCHAR(255),phone VARCHAR(255) UNIQUE,username VARCHAR(255) UNIQUE);")
+        print(cursor.fetchone())
+
+    def create_chat_message_db(self):
+        cursor = self.conn.cursor()
+        cursor.execute("CREATE TABLE ChatMessage (ID INT PRIMARY KEY,senderID INT,chatID INT,text VARCHAR(255),Time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,FOREIGN KEY (senderID) REFERENCES Account(ID),FOREIGN KEY (chatID) REFERENCES Chat(ID));")
+        print(cursor.fetchone())
+
+    def create_group_db(self):
+        cursor = self.conn.cursor()
+        cursor.execute("CREATE TABLE Group (ID INT PRIMARY KEY,Name VARCHAR(255));")
+        print(cursor.fetchone())
+
+    def create_chat_db(self):
+        cursor = self.conn.cursor()
+        cursor.execute("CREATE TABLE Chat (ID INT PRIMARY KEY,firstUserID INT,secondUserID INT,FOREIGN KEY (firstUserID) REFERENCES Account(ID),FOREIGN KEY (secondUserID) REFERENCES Account(ID));")
+        print(cursor.fetchone())
+
+    def create_contact_db(self):
+        cursor = self.conn.cursor()
+        cursor.execute("CREATE TABLE Contacts (ID INT PRIMARY KEY,userID INT,chatID INT,FOREIGN KEY (userID) REFERENCES Account(ID),FOREIGN KEY (chatID) REFERENCES Chat(ID));")
+        print(cursor.fetchone())
+
+    def create_group_message_db(self):
+        cursor = self.conn.cursor()
+        cursor.execute("CREATE TABLE GroupMessage (ID INT PRIMARY KEY,senderID INT,groupID INT,text VARCHAR(255),Time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,FOREIGN KEY (senderID) REFERENCES Account(ID),FOREIGN KEY (groupID) REFERENCES Group(ID));")
+        print(cursor.fetchone())
+
+    def create_group_user_db(self):
+        cursor = self.conn.cursor()
+        cursor.execute("CREATE TABLE GroupUsers (userID INT,groupID INT,FOREIGN KEY (userID) REFERENCES Account(ID),FOREIGN KEY (groupID) REFERENCES Group(ID));")
+        print(cursor.fetchone())
+
+    def create_table(self):
+        self.create_contact_db()
+        self.create_chat_db()
+        self.create_chat_message_db()
+        self.create_group_db()
+        self.create_group_message_db()
+        self.create_group_user_db()
+        self.create_user_db()
