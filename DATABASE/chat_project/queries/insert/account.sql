a6a19eb388e38b9787ddf051000f9bdfcb888855
Abtinz <abtinzandi@gmail.com>
2024-06-03 19:53:54 +0330

    queries

---
+++ DATABASE/chat_project/queries/insert/account.sql
@@ -0,0 +1,11 @@
+INSERT INTO Account (ID, name, fullname, phone, username) VALUES
+(1, 'John', 'John Doe', '555-0101', 'john_doe'),
+(2, 'Jane', 'Jane Smith', '555-0102', 'jane_smith'),
+(3, 'abtin', 'abtin zandi', '0923131232', 'abnz'),
+(4, 'jcob', 'jcob marc', '0922222222', 'jaco'),
+(5, 'marc', 'marc Smith', '58222156', 'marc_smith'),
+(6, 'raty', 'raty raty', '555-011101', 'ratyyyyy'),
+(7, 'nader', 'nader Smith', '555-012202', 'nader'),
+(8, 'leo', 'leo messi', '111111111', 'goat'),
+(9, 'cristiano', 'cristiano ronaldo', '222222222222', 'cr7'),
+(10, 'Janee', 'Janee Smith', '58744156', 'janee_smith');
---
+++ DATABASE/chat_project/queries/qroup.sql
@@ -0,0 +1,5 @@
+
+CREATE TABLE Groups (
+    ID INT PRIMARY KEY,
+    Name VARCHAR(255)
+);
