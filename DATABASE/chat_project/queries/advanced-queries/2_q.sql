37ff8536429483a80c67c5d8e2e8a556e0d06b9a
Abtinz <abtinzandi@gmail.com>
2024-06-29 21:05:06 +0330

    debugging

--- DATABASE/chat_project/queries/advanced-queries/2_q.sql
+++ DATABASE/chat_project/queries/advanced-queries/2_q.sql
@@ -3,7 +3,6 @@ CREATE INDEX idx_account_username ON Account (Username);
 
 CREATE INDEX idx_chatmessage_chatid ON ChatMessage (chatID);
 
-CREATE INDEX idx_groupusers_groupid ON GroupUsers (groupID);
-
+CREATE INDEX idx_groupmessage_groupid ON GroupMessage (groupID);
 
 
