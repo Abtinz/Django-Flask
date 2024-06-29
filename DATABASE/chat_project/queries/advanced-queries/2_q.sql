984bab6ff86543d82c9700b9801abe856742121c
Abtinz <abtinzandi@gmail.com>
2024-06-29 20:59:44 +0330

    index in sql

---
+++ DATABASE/chat_project/queries/advanced-queries/2_q.sql
@@ -0,0 +1,9 @@
+
+CREATE INDEX idx_account_username ON Account (Username);
+
+CREATE INDEX idx_chatmessage_chatid ON ChatMessage (chatID);
+
+CREATE INDEX idx_groupusers_groupid ON GroupUsers (groupID);
+
+
+
