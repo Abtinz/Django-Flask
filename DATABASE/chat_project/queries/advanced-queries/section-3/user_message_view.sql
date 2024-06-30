5b5f9c4ce7310c27ceb1ffe9157bae76e10a424a
Abtinz <abtinzandi@gmail.com>
2024-06-30 16:55:05 +0330

    user_message

--- DATABASE/chat_project/queries/advanced-queries/section-3/user_message_view.sql
+++ DATABASE/chat_project/queries/advanced-queries/section-3/user_message_view.sql
@@ -2,3 +2,6 @@ CREATE VIEW user_message AS
 SELECT cm.ID, cm.text, cm.Time, a.Name AS SenderName, a.Username AS SenderUsername
 FROM ChatMessage cm
 JOIN Account a ON cm.senderID = a.ID;
+
+--this sql will provide useful view for each users messages which have been sent in chat service chat
+-- we have a natural join of account and chatMessages on userID  and the sender ID! now we are able to find receiver and chat texts!
