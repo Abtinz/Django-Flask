ff8276071e5322e2d12ee892b8f456c8532c0086
Abtinz <abtinzandi@gmail.com>
2024-06-30 16:53:36 +0330

    user_message_group

--- DATABASE/chat_project/queries/advanced-queries/section-3/user_group_messages.sql
+++ DATABASE/chat_project/queries/advanced-queries/section-3/user_group_messages.sql
@@ -3,3 +3,6 @@ SELECT gm.ID, gm.text, gm.Time, a.Name AS SenderName, g.Name AS GroupName
 FROM GroupMessage gm
 JOIN Account a ON gm.senderID = a.ID
 JOIN Groups g ON gm.groupID = g.ID;
+
+--this sql will provide useful view for each users messages which have been sent in chat service groups
+-- through natural join of account and GroupMessages on userID and with group id we are able to find user's group messages with their group names
