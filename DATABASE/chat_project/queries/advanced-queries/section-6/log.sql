6559bbfba63a9be822c59ebc7d980fb240d87747
Abtinz <abtinzandi@gmail.com>
2024-06-30 17:06:47 +0330

    funny comments - 2

--- DATABASE/chat_project/queries/advanced-queries/section-6/log.sql
+++ DATABASE/chat_project/queries/advanced-queries/section-6/log.sql
@@ -1,3 +1,9 @@
+--ok now we are going to save logs of users and admins operations!
+-- PK: LogID
+-- TableName, OperationType -> which table is under which operation
+-- OperationTimestamp , OperatorUsername -> who did this operations and when ?
+--changes -> example: user abtin username has changed to abtinz through this operation.
+
 CREATE TABLE AuditLog (
     LogID SERIAL PRIMARY KEY,
     TableName VARCHAR(255),
