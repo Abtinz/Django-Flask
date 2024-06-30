86d97495f2e0ca1148f06bb09d9e70fe02da08b1
Abtinz <abtinzandi@gmail.com>
2024-06-30 17:03:48 +0330

    funny comments

--- DATABASE/chat_project/queries/advanced-queries/section-5/allow.sql
+++ DATABASE/chat_project/queries/advanced-queries/section-5/allow.sql
@@ -1,4 +1,8 @@
+--FIRST I am going to provide an access to DBMS For new administrator(i don't know who is Arousha_Azad,is it safe? i don't even know!!!)
 CREATE USER Arousha2000azad WITH PASSWORD 'Arousha_Azad';
+
+--ok arousha you can access to account now!
 REVOKE SELECT ON account FROM Arousha2000azad;
 
+--you are free to use INSERT, UPDATE, DELETE on account relation(don't disturb it!)
 GRANT INSERT, UPDATE, DELETE ON account TO Arousha2000azad;
