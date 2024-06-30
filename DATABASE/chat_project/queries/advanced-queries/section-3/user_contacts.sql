764cf4f85bbbedcbbfb3509bd6d45d8f0a0cb1d9
Abtinz <abtinzandi@gmail.com>
2024-06-30 16:51:56 +0330

    user contacts

--- DATABASE/chat_project/queries/advanced-queries/section-3/user_contacts.sql
+++ DATABASE/chat_project/queries/advanced-queries/section-3/user_contacts.sql
@@ -2,3 +2,6 @@ CREATE VIEW user_contacts AS
 SELECT a.ID, a.Name, a.Username, a.Phone, c.chatID
 FROM Contacts c
 JOIN Account a ON c.userID = a.ID;
+
+--this sql will provide useful view for each users contacts
+-- through natural join of account and contact on userID we are able to find user's contacts and chats
