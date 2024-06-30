d5b71523596805a4155cee44a44656e9052911ef
Abtinz <abtinzandi@gmail.com>
2024-06-30 16:57:46 +0330

    ensure_unique_phone_number

--- DATABASE/chat_project/queries/advanced-queries/section-4/ensure_unique_phone_number.sql
+++ DATABASE/chat_project/queries/advanced-queries/section-4/ensure_unique_phone_number.sql
@@ -1,3 +1,5 @@
+--here we have a trigger and it's function, when we are inserting or updating users information, we have to check repeated phone assertion!
+
 CREATE OR REPLACE FUNCTION ensure_unique_phone_number()
 RETURNS TRIGGER AS $$
 DECLARE
@@ -5,7 +7,7 @@ DECLARE
 BEGIN
     -- Check if the phone number already exists for another user
     SELECT ID INTO existing_user_id FROM Account WHERE Phone = NEW.Phone AND ID != NEW.ID;
-    IF found THEN
+    IF found THEN --here we have repeated phone number problem! we will send user this alert with its older user id(what a secure way!)
         RAISE EXCEPTION 'Phone number % already exists for user ID %.', NEW.Phone, existing_user_id;
     END IF;
     RETURN NEW;
@@ -15,3 +17,4 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER trigger_unique_phone_number
 BEFORE INSERT OR UPDATE ON Account
 FOR EACH ROW EXECUTE FUNCTION ensure_unique_phone_number();
+
