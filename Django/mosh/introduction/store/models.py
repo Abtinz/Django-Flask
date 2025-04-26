a5517eb01f37b01f6f08a0897375d899f2e09154
Abtinz <abtinzandi@gmail.com>
2025-04-26 13:58:05 +0330

    charfield and choices

--- Django/mosh/introduction/store/models.py
+++ Django/mosh/introduction/store/models.py
@@ -9,9 +9,15 @@ class Product(models):
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
 
-class Customers(models):
+class Customer(models):
+    MEMBERSHIP_CHOICES = [
+        ("B", "Bronze"),
+        ("S", "Silver"),
+        ("G","Gold")
+    ]
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique = True)
     phone = models.CharField(max_length=25)
     birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES)
