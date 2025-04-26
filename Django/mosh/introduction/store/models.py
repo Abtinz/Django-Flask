f7c29120fd68322f6f3cea9f7ec5ec0dfae16752
Abtinz <abtinzandi@gmail.com>
2025-04-26 16:25:07 +0330

    migration with problems ...(default value)

--- Django/mosh/introduction/store/models.py
+++ Django/mosh/introduction/store/models.py
@@ -12,6 +12,7 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
