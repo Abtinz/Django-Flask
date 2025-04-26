7be6c5088cdd1ec4ebe3493340a22ebb5910b9f9
Abtinz <abtinzandi@gmail.com>
2025-04-26 14:04:40 +0330

    Orders practice

--- Django/mosh/introduction/store/models.py
+++ Django/mosh/introduction/store/models.py
@@ -10,14 +10,30 @@ class Product(models):
     last_update = models.DateTimeField(auto_now=True)
 
 class Customer(models):
+
+    MEMBERSHIP_DEFAULT = "B"
     MEMBERSHIP_CHOICES = [
-        ("B", "Bronze"),
+        (MEMBERSHIP_DEFAULT, "Bronze"),
         ("S", "Silver"),
         ("G","Gold")
     ]
+
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique = True)
     phone = models.CharField(max_length=25)
     birth_date = models.DateField(null=True)
-    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default= MEMBERSHIP_DEFAULT)
+
+class Order(models):
+
+    ORDER_STATUS_DEFAULT = "p"
+    ORDER_STATUS_POSSIBLE_STATES = [
+        (ORDER_STATUS_DEFAULT, "Pending"),
+        ("C", "Completed"),
+        ("F","Failed")
+    ]
+
+    placed_at = models.DateTimeField(auto_now_add=True)
+    membership = models.CharField(max_length=1, choices=ORDER_STATUS_POSSIBLE_STATES, default= ORDER_STATUS_DEFAULT)
+
