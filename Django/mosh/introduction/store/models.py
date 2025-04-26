ba91b15873f73584de6da87a4814212f0fb7d812
Abtinz <abtinzandi@gmail.com>
2025-04-26 16:22:51 +0330

    migration

--- Django/mosh/introduction/store/models.py
+++ Django/mosh/introduction/store/models.py
@@ -13,7 +13,7 @@ class Collection(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits= 6,decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
