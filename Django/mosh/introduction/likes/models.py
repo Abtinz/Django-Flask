607ae6ba831ab698867dfe0edf353f4089d023b7
Abtinz <abtinzandi@gmail.com>
2025-04-26 15:18:27 +0330

    the definition of generic contents in django models

--- Django/mosh/introduction/likes/models.py
+++ Django/mosh/introduction/likes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+class LikedItem(models.Model):
+    user = models.ForeignKey(User , on_delete= models.CASCADE)
+    #Generic content type, id, object
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveSmallIntegerField()
+    content_object = GenericForeignKey()
--- Django/mosh/introduction/tags/models.py
+++ Django/mosh/introduction/tags/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+class Tag(models.Model):
+    label = models.CharField(unique=True , max_length=255)
+
+class TagItem(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    #Generic content type, id, object
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveSmallIntegerField()
+    content_object = GenericForeignKey()
