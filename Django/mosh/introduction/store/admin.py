d627fa130d95db2beff27a4d17d624a64ebc2000
Abtinz <abtinzandi@gmail.com>
2025-04-28 00:06:01 +0330

    making new column for admin display

--- Django/mosh/introduction/store/admin.py
+++ Django/mosh/introduction/store/admin.py
@@ -3,8 +3,16 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory']
-    list_editable = ['unit_price', 'inventory']
+    list_display = ['title', 'unit_price', 'inventory_status']
+    list_editable = ['unit_price']
+
+    @admin.display(ordering="inventory")
+    def inventory_status(self, product):
+        if product.inventory > 10:
+            return 'OK +'
+        elif product.inventory > 0:
+            return 'Low'
+        return 'Out of Stock'
 
 admin.site.register(models.Collection)
 admin.site.register(models.Customer)
