dd1bc916c907aaa325ecc2806beef3734b35669f
Abtinz <abtinzandi@gmail.com>
2025-04-27 18:26:11 +0330

    admin - headers

--- Django/mosh/introduction/storefront/urls.py
+++ Django/mosh/introduction/storefront/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
 
+admin.sites.site.site_header = "Store Project - Django with Mosh - Admin Panel"
+admin.sites.site.index_title = "Admin - 01"
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("playground/", include("playground.urls"))
