e7f09afcc2d2a4a27772fd4e5df7fc1ec9aadcbf
Abtinz <abtinzandi@gmail.com>
2025-04-27 18:36:03 +0330

    admin - class Admin + list display

--- Django/mosh/introduction/store/admin.py
+++ Django/mosh/introduction/store/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from . import models
 
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price', 'inventory']
+
 admin.site.register(models.Collection)
-admin.site.register(models.Product)
 admin.site.register(models.Customer)
 admin.site.register(models.OrderItem)
