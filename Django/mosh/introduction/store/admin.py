ae05ab544eeafb3ca4f0deb19475093df064103d
Abtinz <abtinzandi@gmail.com>
2025-05-08 13:54:00 +0330

    links in admin panel

--- Django/mosh/introduction/store/admin.py
+++ Django/mosh/introduction/store/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from . import models
+from django.db import models
+from django.urls import reverse
+from django.utils.html import format_html, urlencode
+from .models import Customer, OrderItem, Collection, Product
 
-@admin.register(models.Product)
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'unit_price', 'inventory_status']
     list_editable = ['unit_price']
@@ -14,6 +17,23 @@ class ProductAdmin(admin.ModelAdmin):
             return 'Low'
         return 'Out of Stock'
 
-admin.site.register(models.Collection)
-admin.site.register(models.Customer)
-admin.site.register(models.OrderItem)
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'products_count']
+
+    @admin.display(ordering="products_count")
+    def products_count(self, collection):
+        url = ( reverse("admin:store_product_changelist")+ "?"+ urlencode({"collection__id" :  str(collection.id)}) )
+
+        return format_html(
+            "<a href={}>{}<a>", url, collection.products_count
+        )
+        
+    def get_queryset(self, request):
+        return super().get_queryset(request)\
+            .annotate(
+                products_count = models.Count('product')
+            )
+
+admin.site.register(Customer)
+admin.site.register(OrderItem)
