8110fda277f58a0d123a52bd49f9607b4630116b
Abtinz <abtinzandi@gmail.com>
2025-05-09 22:57:48 +0330

    source and renaming in serializers

--- Django/mosh/introduction/store/serializers.py
+++ Django/mosh/introduction/store/serializers.py
@@ -7,7 +7,8 @@ class ProductSerializer(serializers.Serializer):
     '''This is products serializer for GET method(a simple test for getting a list of products in django-rest-framework)'''
     id = serializers.IntegerField()
     title = serializers.CharField(max_length = 255)
-    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+    #source and renaming in serializers
+    price = serializers.DecimalField(max_digits=6, decimal_places=2,source= 'unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
 
     def calculate_tax(self, product:Product):
