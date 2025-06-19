d9ab0a1c9e8e342b61191863f0ba9552b215787d
Abtinz <abtinzandi@gmail.com>
2025-06-19 17:18:56 +0330

    admin ordering

--- Django/mosh/introduction/store/models.py
+++ Django/mosh/introduction/store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib import admin
 from django.conf import settings
 from uuid import uuid4 # we will use it for default value of card uuid
 
@@ -102,9 +103,11 @@ class Customer(models.Model):
     def __str__(self):
         return self.user.email
 
+    @admin.display(ordering= 'user__first_name')
     def first_name(self):
         return self.user.first_name
     
+    @admin.display(ordering= 'user__last_name')
     def last_name(self):
         return self.user.last_name
     
