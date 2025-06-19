08249c3074f9cb7ac2b1ed7d408d07aef2b7d44c
Abtinz <abtinzandi@gmail.com>
2025-06-19 21:10:43 +0330

    token life time

--- Django/mosh/introduction/storefront/settings.py
+++ Django/mosh/introduction/storefront/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -150,6 +151,8 @@ REST_FRAMEWORK = {
 #DJOSER authentication configs -> headers ... 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPE': ('JWT',),
+    #token life time definition using timedelta
+    'ACCESS_TOKEN_LIFETIME' : timedelta(days=365)
 }
 
 #If we extend default user's abstract class from django.auth, we should notify
